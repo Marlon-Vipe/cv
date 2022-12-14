190c3c0388cec3d3581c00592ea37d47c0f873cf
Marlon Villalona <iamvillalona@gmail.com>
2022-12-14 13:47:50 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const creditos = 520
+
+
+const creditosCursados = prompt('Cuantos creditos va a cursar?');
+
+let result = creditos * creditosCursados;
+
+alert('Tendras que pagar un total de RD$' + result + '.00 pesos');
+
